c3da56485669ded187fa87bd6ba2cd6f7e084357
gdjwixgdj <nachnanideepak831@gmail.com>
2025-05-06 17:03:18 +0530

    Update Assignment3(BST).cpp

--- Assignment3(BST).cpp
+++ Assignment3(BST).cpp
@@ -1,182 +1,189 @@
+
 #include <iostream>
 using namespace std;
-
-class Node {
-public:
-    int data;
-    Node *left, *right;
+class Node
+{
+        int data;
+        Node* left;
+        Node* right;
+    public:
+        Node()
+        {
+            data = 0;
+            left = right = NULL;
+        }
+        Node(int d)
+        {
+            data = d;
+            left = right = NULL;
+        }
+        friend class BST;
 };
+class BST
+{
+        Node* root;
+    public:
+        BST()
+        {
+            root = NULL;
+        }
+        Node* getRoot()
+        {
+            return root;
+        }
+        void insert(int d)
+        {
+            Node* p = new Node(d);
+            if(root == NULL)
+            {
+                root = p;
+                return;
+            }
+            Node* q = root;
+            while(q != NULL)
+            {
+                if(d < q -> data)
+                {
+                    if(q -> left == NULL)
+                    {
+                        q -> left = p;
+                        break;
+                    }
+                    q = q -> left;
+                }
+                else if(d > q -> data)
+                {
+                    if(q -> right == NULL)
+                    {
+                        q -> right = p;
+                        break;
+                    }
+                    q = q -> right; 
+                }
+            }
+        }
+        void mirror(Node* root)
+        {
+            if(root != NULL)
+            {
+                Node* temp = root -> left;
+                root -> left = root -> right;
+                root -> right = temp;
 
-class BST {
-public:
-    Node* root;
-
-    BST() {
-        root = NULL;
-    }
-
-    Node* createNode(int val) {
-        Node* newNode = new Node();
-        newNode->data = val;
-        newNode->left = NULL;
-        newNode->right = NULL;
-        return newNode;
-    }
-
-    void insert(int val) {
-        Node* newNode = createNode(val);
-        if (root == NULL) {
-            root = newNode;
-            cout << "Node inserted.\n";
+                mirror(root->left);
+                mirror(root->right);
+            }
             return;
         }
-        Node* temp = root;
-        while (true) {
-            if (val < temp->data) {
-                if (temp->left == NULL) {
-                    temp->left = newNode;
-                    cout << "Node inserted.\n";
-                    break;
-                } else {
-                    temp = temp->left;
-                }
-            } else if (val > temp->data) {
-                if (temp->right == NULL) {
-                    temp->right = newNode;
-                    cout << "Node inserted.\n";
-                    break;
-                } else {
-                    temp = temp->right;
-                }
-            } else {
-                cout << "Value already exists.\n";
-                break;
+        void minimum()
+        {
+            if(root == NULL)
+            {
+                cout << "BST is empty";
+                return;
             }
-        }
-    }
+            Node* q = root;
+            while(q -> left)
+                q = q -> left;
 
-    void inorder(Node* temp) {
-        if (temp != NULL) {
-            inorder(temp->left);
-            cout << temp->data << " ";
-            inorder(temp->right);
+            cout << "Minimum element : " << q -> data << endl;
         }
-    }
+        int height(Node* root)
+        {
+            int leftHeight, rightHeight;
+            if(root == NULL || root->left == NULL && root->right == NULL)
+                return 0;
 
-    void preorder(Node* temp) {
-        if (temp != NULL) {
-            cout << temp->data << " ";
-            preorder(temp->left);
-            preorder(temp->right);
-        }
-    }
+            leftHeight = height(root->left);
+            rightHeight = height(root->right);
 
-    int findMin() {
-        if (root == NULL) {
-            return -1;
+            if(leftHeight > rightHeight)
+                return leftHeight + 1;
+            else
+                return rightHeight + 1;
         }
-        Node* temp = root;
-        while (temp->left != NULL) {
-            temp = temp->left;
-        }
-        return temp->data;
-    }
+        void search()
+        {
+            int key;
+            cout << "Enter value to search : ";
+            cin >> key;
 
-    int findMax() {
-        if (root == NULL) {
-            return -1;
-        }
-        Node* temp = root;
-        while (temp->right != NULL) {
-            temp = temp->right;
-        }
-        return temp->data;
-    }
-
-    void mirror(Node* temp) {
-        if (temp != NULL) {
-            Node* swapNode = temp->left;
-            temp->left = temp->right;
-            temp->right = swapNode;
-
-            mirror(temp->left);
-            mirror(temp->right);
-        }
-    }
-
-    bool search(int key) {
-        Node* temp = root;
-        while (temp != NULL) {
-            if (key < temp->data) {
-                temp = temp->left;
-            } else if (key > temp->data) {
-                temp = temp->right;
-            } else {
-                return true;
+            Node* q = root;
+            while(q)
+            {
+                if(key < q -> data)
+                    q = q -> left;
+                else if(key > q -> data)
+                    q = q -> right;
+                else
+                {
+                    cout << key << " found in BST" << endl;
+                    return;
+                }
             }
+            cout << key << " not found" << endl;
         }
-        return false;
-    }
-
-    int height(Node* temp) {
-        if (temp == NULL) {
-            return 0;
+        void inorder(Node* root)
+        {
+            if(root == NULL)
+                return;
+            inorder(root->left);
+            cout << root->data << " ";
+            inorder(root->right);
         }
-        int leftHeight = height(temp->left);
-        int rightHeight = height(temp->right);
-        if (leftHeight > rightHeight)
-            return leftHeight + 1;
-        else
-            return rightHeight + 1;
-    }
+        void display()
+        {
+            cout << "BST elements are (Inorder)" << endl;
+            inorder(root);
+            cout << endl;
+        }
 };
+int main()
+{
+    BST bst;
+    bool flag = true;
+    int choice, key;
 
-int main() {
-    BST tree;
-    int choice, val;
-    do {
-        cout << "\n1. Insert\n2. Inorder\n3. Preorder\n4. Minimum\n5. Maximum\n6. Mirror\n7. Search\n8. Height\n9. Exit\nEnter choice: ";
+    while(flag)
+    {
+        cout << "1. Insert new Node" << endl;
+        cout << "2. Height of BST" << endl; 
+        cout << "3. Minimum Value" << endl;
+        cout << "4. Mirror" << endl;
+        cout << "5. Search a value" << endl;
+        cout << "6. Display" << endl;
+        cout << "7. Exit" << endl;
+        cout << "Enter your choice : ";
         cin >> choice;
-        if (choice == 1) {
-            cout << "Enter value: ";
-            cin >> val;
-            tree.insert(val);
-        } else if (choice == 2) {
-            cout << "Inorder: ";
-            tree.inorder(tree.root);
-            cout << endl;
-        } else if (choice == 3) {
-            cout << "Preorder: ";
-            tree.preorder(tree.root);
-            cout << endl;
-        } else if (choice == 4) {
-            int minVal = tree.findMin();
-            if (minVal == -1)
-                cout << "Tree is empty.\n";
-            else
-                cout << "Minimum: " << minVal << endl;
-        } else if (choice == 5) {
-            int maxVal = tree.findMax();
-            if (maxVal == -1)
-                cout << "Tree is empty.\n";
-            else
-                cout << "Maximum: " << maxVal << endl;
-        } else if (choice == 6) {
-            tree.mirror(tree.root);
-            cout << "Tree mirrored. Inorder: ";
-            tree.inorder(tree.root);
-            cout << endl;
-        } else if (choice == 7) {
-            cout << "Enter value to search: ";
-            cin >> val;
-            if (tree.search(val))
-                cout << "Found.\n";
-            else
-                cout << "Not found.\n";
-        } else if (choice == 8) {
-            cout << "Height: " << tree.height(tree.root) << endl;
-        }
-    } while (choice != 9);
 
-    return 0;
+        switch(choice)
+        {
+            case 1:
+                cout << "Enter data of node : ";
+                cin >> key;
+                bst.insert(key);
+                break;
+            case 2:
+                cout << "Height of BST : " << bst.height(bst.getRoot()) << endl;
+                break;
+            case 3:
+                bst.minimum();
+                break;
+            case 4:
+                bst.mirror(bst.getRoot());
+                break;
+            case 5:
+                bst.search();
+                break;
+            case 6:
+                bst.display();
+                break;
+            case 7:
+                cout << "End of Program" << endl;
+                flag = false;
+                break;
+            default:
+                cout << "Invalid Choice!" << endl;
+        }
+    }
 }
